ede622b1ae3c4e3a1e75d0e18c9e867da422464b
Didar Auyesbay <didarmaratov04@gmail.com>
2024-05-13 05:03:43 +0500

    emailjs added for feedback

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
+import emailjs from "@emailjs/browser";
 
 export default function Feedback() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
+    const [isSent, setIsSent] = useState(false);
 
-    const handleSubmit = (event) => {
+    useEffect(() => emailjs.init(import.meta.env.VITE_EMAILJS_PUBLIC_KEY), []);
+
+    const form = useRef();
+
+    const sendEmail = (event) => {
         event.preventDefault();
         // Handle form submission
         console.log("Name:", name);
@@ -15,26 +21,42 @@ export default function Feedback() {
         setName("");
         setEmail("");
         setMessage("");
+
+        emailjs.sendForm(import.meta.env.VITE_EMAILJS_SERVICE_ID, import.meta.env.VITE_EMAILJS_TEMPLATE_ID, form.current, import.meta.env.VITE_EMAILJS_PUBLIC_KEY).then(
+            (result) => {
+                console.log("Email sent successfully", result);
+                setIsSent(true);
+            },
+            (error) => {
+                console.log("Error sending email:", error);
+            }
+        );
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div className="mx-auto flex flex-col p-8">
+        <form ref={form} onSubmit={sendEmail}>
+            <div className="mx-auto flex flex-col py-2">
+                {isSent && (
+                    <div className="py-1 my-2 text-center border bg-green-400 rounded">
+                        <p>Thank you for your feedback!</p>
+                    </div>
+                )}
                 <div className="mb-4">
                     <label className="text-sm">Name</label>
-                    <input type="name" id="name" value={name} name="name" className="w-full py-1 px-3 rounded border border-gray-300 text-black" onChange={(e) => setName(e.target.value)} />
+                    <input type="name" id="name" value={name} name="from_name" className="w-full py-1 px-3 rounded border border-gray-300 text-black" onChange={(e) => setName(e.target.value)} />
                 </div>
                 <div className="mb-4">
                     <label className="text-sm">Email</label>
-                    <input type="email" id="email" value={email} name="email" className="w-full py-1 px-3 rounded border border-gray-300 text-black" onChange={(e) => setEmail(e.target.value)} />
+                    <input type="email" id="email" value={email} name="from_email" className="w-full py-1 px-3 rounded border border-gray-300 text-black" onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div className="mb-4">
                     <label className="text-sm">Message</label>
                     <textarea id="message" value={message} name="message" className="h-32 py-1 px-3 w-full resize-none rounded border border-gray-300 text-black" onChange={(e) => setMessage(e.target.value)}></textarea>
                 </div>
-                <button className="rounded border-0 bg-indigo-500 py-2 px-6 text-lg text-white hover:bg-indigo-600 focus:outline-none" onClick={handleSubmit}>
+                <button className="rounded border-0 bg-indigo-500 py-2 px-6 text-lg text-white hover:bg-indigo-600 focus:outline-none" onClick={sendEmail}>
                     Send
                 </button>
+                {/* {stateMessage && <p>{stateMessage}</p>} */}
                 <p className="mt-3 text-xs text-gray-500">Feel free to connect with me on social media platforms</p>
             </div>
         </form>
