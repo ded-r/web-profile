71adbd730b7fd4792f935ce939ee70478f6c6179
Didar Auyesbay <didarmaratov04@gmail.com>
2024-11-22 12:27:24 +0500

    refactoring

--- src/constants/technologies.jsx
+++ src/constants/technologies.jsx
@@ -13,4 +13,5 @@ const Technologies = [
     { name: "Kubernetes", logo: "https://img.shields.io/badge/kubernetes-%23326ce5.svg?style=for-the-badge&logo=kubernetes&logoColor=white" },
 ];
 
+
 export default Technologies;
